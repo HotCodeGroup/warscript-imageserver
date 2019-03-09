9c48e22ce635fbdb00efd4dce9c2b99515e5f4fc
GDVFox <gaaveer@gmail.com>
2019-03-09 21:06:18 +0300

    [UPD] Cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/HotCodeGroup/warscript-imageserver/controllers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 
 	log "github.com/sirupsen/logrus"
@@ -21,9 +22,16 @@ func main() {
 	r.HandleFunc("/photos", controllers.UploadPhoto).Methods("POST")
 	r.HandleFunc("/photos/{photo_uuid}", controllers.GetPhoto).Methods("GET")
 	r.HandleFunc("/", controllers.MainPage).Methods("GET")
+	corsMiddleware := handlers.CORS(
+		handlers.AllowedOrigins([]string{os.Getenv("CORS_HOST")}),
+		handlers.AllowedMethods([]string{"POST", "GET", "PUT", "DELETE"}),
+		handlers.AllowedHeaders([]string{"Content-Type"}),
+		handlers.AllowCredentials(),
+	)
+
 	port := os.Getenv("PORT")
 	log.Infof("MainService successfully started at port %s", port)
-	err := http.ListenAndServe(":"+port, r)
+	err := http.ListenAndServe(":"+port, corsMiddleware(r))
 	if err != nil {
 		log.Errorf("cant start main server. err: %s", err.Error())
 		return
